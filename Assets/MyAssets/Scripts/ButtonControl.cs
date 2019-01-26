879bb5a4945bc63fe262af0e7f81e6926ebdbd2e
Matteo D'Ambra <ivhol@hotmail.it>
2019-01-26 14:03:18 +0100

    - main menu

---
+++ Assets/MyAssets/Scripts/ButtonControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonControl : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
