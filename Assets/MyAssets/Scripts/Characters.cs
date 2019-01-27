7423f0fc080f23ca6916eee1c6c1b461f1de3942
Falci Angelo <a.falci@live.it>
2019-01-27 10:21:30 +0100

    menu update

---
+++ Assets/MyAssets/Scripts/Characters.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Characters : MonoBehaviour { 
+
+    public float SpeedSlider = 5;
+
+    private GameObject Panels;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Panels = transform.Find("Panels").gameObject;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 p=Panels.transform.position;
+        p.x -= SpeedSlider;
+        Panels.transform.position = p;
+    }
+}
