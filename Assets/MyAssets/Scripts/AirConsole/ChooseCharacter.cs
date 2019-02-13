d1d512b1bc5dc7121467c633df48d227330fd932
Falci Angelo <a.falci@live.it>
2019-02-13 23:32:42 +0100

    choose player work in progress

---
+++ Assets/MyAssets/Scripts/AirConsole/ChooseCharacter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class ChooseCharacter : MonoBehaviour
+{
+    [Header("Player ID")]
+    public int PlayerID;
+    [Header("Choose Player Component")]
+    public GameObject Player;
+
+    private int IndexPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IndexPlayer = 0;
+    }
+
+    //AirConsole Functions
+    void OnMessage(int device_id, JToken data)
+    {
+        int active_player = AirConsole.instance.ConvertDeviceIdToPlayerNumber(device_id);
+
+        if (active_player == PlayerID)
+        {
+            if (data != null &&
+                data["key"] != null &&
+                data["pressed"] != null)
+            {
+                string key = key = (string)data["key"];
+                if ((bool)data["pressed"])
+                {
+                    Debug.Log("IF");
+                    if (key.Equals("right"))
+                    {
+                    }
+                    if (key.Equals("left"))
+                    {
+                    }
+                    if (key.Equals("confirm"))
+                    {
+                    }
+                }
+            }
+        }
+    }
+
+    private void ChangeCharacter()
+    {
+
+    }
+
+    private void ChoosePlayer()
+    {
+        ChooseCharacterManager.instance.ConfirmPlayer();
+    }
+}
---
+++ Assets/MyAssets/Scripts/AirConsole/ChooseCharacterManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class ChooseCharacterManager : MonoBehaviour
+{
+    public static ChooseCharacterManager instance;
+
+    private int CountConfirm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+        CountConfirm = 0;
+    }
+
+    public void ConfirmPlayer()
+    {
+        CountConfirm++;
+        int totalPlayer = AirConsole.instance.GetActivePlayerDeviceIds.Count;
+        if (CountConfirm == totalPlayer)
+        {//Start the game
+
+        }
+    }
+}
