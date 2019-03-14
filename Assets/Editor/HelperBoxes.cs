25fdc09e1c5dd9a1f2c4029081f209c0a89ddbb1
Falci Angelo <a.falci@live.it>
2019-03-14 23:25:18 +0100

    start to add help box to explain single script

---
+++ Assets/Editor/HelperBoxes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+
+[CustomEditor(typeof(Player_AirConsole))]
+public class LevelScriptEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //Draw new components we want to add in the inspector
+        EditorGUILayout.HelpBox("Script that controller a single player, included his movements, collisions and score", MessageType.Info);
+        //Draw the default inspector
+        DrawDefaultInspector();
+        
+    }
+}
